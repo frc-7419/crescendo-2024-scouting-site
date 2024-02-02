e1f95c0209d477d0dcbfa0cdcda7e80ed955b204
Jolteous <wirelesskhurram@outlook.com>
2024-02-01 16:27:28 -0800

    small css update

--- app/robot/Form.client.tsx
+++ app/robot/Form.client.tsx
@@ -62,31 +62,30 @@ const Form = () => {
     };
 
     return (
-        <div className = 'form'> 
-        <form onSubmit={handleSubmit}>
-            <input type="text" value={name} onChange={handleNameChange} placeholder="Scouter Name" maxLength={5} />          
-            <input type="text" value={robotNumber} onChange={handleRobotNumberChange} placeholder="Robot Number" maxLength={5} />          
-            <input type="text" value={matchNumber} onChange={handleMatchNumberChange} placeholder="Match Number" maxLength={2}/>
+    <div className="form bg-blue-500 shadow-md rounded px-8 pt-6 pb-8 mb-4"> 
+    <form onSubmit={handleSubmit}>
+        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" type="text" value={name} onChange={handleNameChange} placeholder="Scouter Name" maxLength={5} />          
+        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" type="text" value={robotNumber} onChange={handleRobotNumberChange} placeholder="Robot Number" maxLength={5} />          
+        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" type="text" value={matchNumber} onChange={handleMatchNumberChange} placeholder="Match Number" maxLength={2}/>
 
-            <h3 className="text-2xl bold m-3">Auto</h3>
-            <input type="number" value={autoAmps} onChange={handleAutoAmpsChange} placeholder="Amps in Auto" className=""/>
-            <input type="number" value={autoSpeakers} onChange={handleAutoSpeakersChange} placeholder="Speakers in Auto" className=""/>
-            <h3 className="text-2xl bold m-3">Teleop</h3>
-            <input type="number" value={teleopAmps} onChange={handleTeleopAmpsChange} placeholder="Amps in Teleop" className=""/>
-            <input type="number" value={teleopSpeakers} onChange={handleTeleopSpeakersChange} placeholder="Speakers in Teleop" className=""/>
-            <h3 className="text-2xl bold m-3">Endgame</h3>
-            <div className='block items-center'>
-            <label htmlFor="trap" className="p-2 text-xl font-medium text-black dark:text-gray-300">Trap?</label>
-            <input type="checkbox" id="trap" checked={trap} onChange={handleTrapChange} className="w-5 h-5 p-5 m-3 text-blue-600 hover:bg-gray-300 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-            <label htmlFor="hang" className="p-2 text-xl font-medium text-black dark:text-gray-300">Hang?</label>
-            <input type="checkbox" id="hang" checked={hang} onChange={handleHangChange} className="w-5 h-5 p-5 m-3 text-blue-600 hover:bg-gray-300 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-            <label htmlFor="spotlight" className="p-2 text-xl font-medium text-black dark:text-gray-300">Spotlight?</label>
-            <input type="checkbox" id="spotlight" checked={spotlight} onChange={handleSpotlightChange} className="w-5 h-5 p-5 m-3 text-blue-600 hover:bg-gray-300 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-            </div>
-            <button type="submit">Submit</button>
-        </form>
+        <h3 className="text-2xl font-bold m-3 text-white">Auto</h3>
+        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" type="number" value={autoAmps} onChange={handleAutoAmpsChange} placeholder="Amps in Auto" />
+        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" type="number" value={autoSpeakers} onChange={handleAutoSpeakersChange} placeholder="Speakers in Auto" />
+        <h3 className="text-2xl font-bold m-3 text-white">Teleop</h3>
+        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" type="number" value={teleopAmps} onChange={handleTeleopAmpsChange} placeholder="Amps in Teleop" />
+        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" type="number" value={teleopSpeakers} onChange={handleTeleopSpeakersChange} placeholder="Speakers in Teleop" />
+        <h3 className="text-2xl font-bold m-3 text-white">Endgame</h3>
+        <div className='block items-center'>
+        <label htmlFor="trap" className="p-2 text-xl font-medium text-white dark:text-gray-300">Trap?</label>
+        <input type="checkbox" id="trap" checked={trap} onChange={handleTrapChange} className="w-5 h-5 p-5 m-3 text-blue-600 hover:bg-gray-300 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+        <label htmlFor="hang" className="p-2 text-xl font-medium text-white dark:text-gray-300">Hang?</label>
+        <input type="checkbox" id="hang" checked={hang} onChange={handleHangChange} className="w-5 h-5 p-5 m-3 text-blue-600 hover:bg-gray-300 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+        <label htmlFor="spotlight" className="p-2 text-xl font-medium text-white dark:text-gray-300">Spotlight?</label>
+        <input type="checkbox" id="spotlight" checked={spotlight} onChange={handleSpotlightChange} className="w-5 h-5 p-5 m-3 text-blue-600 hover:bg-gray-300 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
         </div>
-    );
-};
-
+        <button type="submit" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Submit</button>
+    </form>
+    </div>
+);
+ }
 export default Form;
