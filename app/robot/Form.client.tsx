2a24f8e9a52b79acb48aa3b3d71c685d759cdbcc
Jolteous <wirelesskhurram@outlook.com>
2024-02-01 16:54:38 -0800

    json

--- app/robot/Form.client.tsx
+++ app/robot/Form.client.tsx
@@ -1,91 +1,58 @@
 "use client";
 
+
 import React, { useState } from 'react';
-import "../globals.css";
 
 const Form = () => {
-    const [name, setName] = useState('');
-    const [robotNumber, setRobotNumber] = useState('');
-    const [matchNumber, setMatchNumber] = useState('');
-    const [autoAmps, setAutoAmps] = useState('');
-    const [autoSpeakers, setAutoSpeakers] = useState('');
-    const [teleopAmps, setTeleopAmps] = useState('');
-    const [teleopSpeakers, setTeleopSpeakers] = useState('');
-    const [trap, setTrap] = useState(false);
-    const [hang, setHang] = useState(false);
-    const [spotlight, setSpotlight] = useState(false);
+    const [formState, setFormState] = useState({
+        name: '',
+        robotNumber: '',
+        matchNumber: '',
+        autoAmps: 0,
+        autoSpeakers: 0,
+        teleopAmps: 0,
+        teleopSpeakers: 0,
+        trap: false,
+        hang: false,
+        spotlight: false
+    });
 
-    const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setRobotNumber(event.target.value);
-    };
-
-    const handleRobotNumberChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setRobotNumber(event.target.value);
-    };
-
-    const handleMatchNumberChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setMatchNumber(event.target.value);
-    };
-
-    const handleAutoAmpsChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setAutoAmps(event.target.value);
-    };
-
-    const handleAutoSpeakersChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setAutoSpeakers(event.target.value);
-    };
-
-    const handleTeleopAmpsChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setTeleopAmps(event.target.value);
-    };
-
-    const handleTeleopSpeakersChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setTeleopSpeakers(event.target.value);
-    };
-
-    const handleTrapChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setTrap(event.target.checked);
-    };
-    
-    const handleHangChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setHang(event.target.checked);
-    };
-
-    const handleSpotlightChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setSpotlight(event.target.checked);
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
+        setFormState({
+            ...formState,
+            [event.target.name]: value
+        });
     };
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        console.log('Robot Number:', robotNumber);
-        console.log('Match Number:', matchNumber);
+        console.log(formState);
     };
 
     return (
-    <div className="form bg-blue-500 shadow-md rounded px-8 pt-6 pb-8 mb-4"> 
-    <form onSubmit={handleSubmit}>
-        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" type="text" value={name} onChange={handleNameChange} placeholder="Scouter Name" maxLength={5} />          
-        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" type="text" value={robotNumber} onChange={handleRobotNumberChange} placeholder="Robot Number" maxLength={5} />          
-        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" type="text" value={matchNumber} onChange={handleMatchNumberChange} placeholder="Match Number" maxLength={2}/>
+        <div className="form bg-blue-500 shadow-md rounded px-8 pt-6 pb-8 mb-4"> 
+            <form onSubmit={handleSubmit}>
+                <input name="name" className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" type="text" value={formState.name} onChange={handleInputChange} placeholder="Scouter Name" maxLength={10} />          
+                <input name="robotNumber" className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" type="text" value={formState.robotNumber} onChange={handleInputChange} placeholder="Robot Number" maxLength={5} />          
+                <input name="matchNumber" className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" type="text" value={formState.matchNumber} onChange={handleInputChange} placeholder="Match Number" maxLength={2}/>
+                <input name="autoAmps" className="shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline" type="number" value={formState.autoAmps || ''} onChange={handleInputChange} placeholder="Amps in Auto" />
+                <input name="autoSpeakers" className="shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline" type="number" value={formState.autoSpeakers || ''} onChange={handleInputChange} placeholder="Speakers in Auto" />
+                <input name="teleopAmps" className="shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline" type="number" value={formState.teleopAmps || ''} onChange={handleInputChange} placeholder="Amps in Teleop" />
+                <input name="teleopSpeakers" className="shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline" type="number" value={formState.teleopSpeakers || ''} onChange={handleInputChange} placeholder="Speakers in Teleop" />
+                
+               <h1 style={{ fontFamily: 'Arial', color: 'white', fontSize: '1.5em', marginBottom: '10px' }}>Trap</h1>
+<input name="trap" className="border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline" type="checkbox" checked={formState.trap} onChange={handleInputChange} />
 
-        <h3 className="text-2xl font-bold m-3 text-white">Auto</h3>
-        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" type="number" value={autoAmps} onChange={handleAutoAmpsChange} placeholder="Amps in Auto" />
-        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" type="number" value={autoSpeakers} onChange={handleAutoSpeakersChange} placeholder="Speakers in Auto" />
-        <h3 className="text-2xl font-bold m-3 text-white">Teleop</h3>
-        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" type="number" value={teleopAmps} onChange={handleTeleopAmpsChange} placeholder="Amps in Teleop" />
-        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline" type="number" value={teleopSpeakers} onChange={handleTeleopSpeakersChange} placeholder="Speakers in Teleop" />
-        <h3 className="text-2xl font-bold m-3 text-white">Endgame</h3>
-        <div className='block items-center'>
-        <label htmlFor="trap" className="p-2 text-xl font-medium text-white dark:text-gray-300">Trap?</label>
-        <input type="checkbox" id="trap" checked={trap} onChange={handleTrapChange} className="w-5 h-5 p-5 m-3 text-blue-600 hover:bg-gray-300 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-        <label htmlFor="hang" className="p-2 text-xl font-medium text-white dark:text-gray-300">Hang?</label>
-        <input type="checkbox" id="hang" checked={hang} onChange={handleHangChange} className="w-5 h-5 p-5 m-3 text-blue-600 hover:bg-gray-300 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-        <label htmlFor="spotlight" className="p-2 text-xl font-medium text-white dark:text-gray-300">Spotlight?</label>
-        <input type="checkbox" id="spotlight" checked={spotlight} onChange={handleSpotlightChange} className="w-5 h-5 p-5 m-3 text-blue-600 hover:bg-gray-300 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+<h1 style={{ fontFamily: 'Arial', color: 'white', fontSize: '1.5em', marginBottom: '10px' }}>Hang</h1>
+<input name="hang" className="border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline" type="checkbox" checked={formState.hang} onChange={handleInputChange} />
+
+<h1 style={{ fontFamily: 'Arial', color: 'white', fontSize: '1.5em', marginBottom: '10px' }}>Spotlight</h1>
+<input name="spotlight" className="border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline" type="checkbox" checked={formState.spotlight} onChange={handleInputChange} />
+                <button type="submit" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Submit</button>
+            </form>
         </div>
-        <button type="submit" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Submit</button>
-    </form>
-    </div>
-);
- }
+    );
+};
+
 export default Form;
