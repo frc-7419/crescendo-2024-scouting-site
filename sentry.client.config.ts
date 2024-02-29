9151f6314e791d53d2c64dca2fab49f268b402a1
MostOriginalIGN <astrogamer543@gmail.com>
2024-02-28 16:21:20 -0800

    i think it does it automatically but it works

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -14,7 +14,7 @@ Sentry.init({
     },
 
     // Adjust this value in production, or use tracesSampler for greater control
-    tracesSampleRate: 1,
+    tracesSampleRate: 1.0,
 
     // Setting this option to true will print useful information to the console while you're setting up Sentry.
     debug: false,
@@ -33,5 +33,6 @@ Sentry.init({
             blockAllMedia: true,
         }),
         Sentry.feedbackIntegration(),
+        Sentry.browserTracingIntegration()
     ],
 });
