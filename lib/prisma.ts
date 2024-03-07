0fff1bee324f1b3b658c2a127a06e770badb5988
MostOriginalIGN <astrogamer543@gmail.com>
2024-03-07 15:54:04 -0800

    no idea how to setup dev env for prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,5 @@
 import {PrismaClient} from '@prisma/client'
+import {withAccelerate} from '@prisma/extension-accelerate'
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
-
-const prisma =
-    globalForPrisma.prisma || new PrismaClient()
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-
+const prisma = new PrismaClient().$extends(withAccelerate())
 export default prisma;
