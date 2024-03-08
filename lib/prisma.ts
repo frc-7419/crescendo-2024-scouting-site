41a8b782d7d0b421ca136d444b7fca2eaa476049
MostOriginalIGN <astrogamer543@gmail.com>
2024-03-07 23:54:28 -0800

    pls fix

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,16 @@
 import {PrismaClient} from '@prisma/client'
-import {withAccelerate} from '@prisma/extension-accelerate'
+import {withAccelerate} from "@prisma/extension-accelerate";
 
-const prisma = new PrismaClient().$extends(withAccelerate())
-export default prisma;
+const prismaClient = () => {
+    return new PrismaClient().$extends(withAccelerate())
+}
+
+declare global {
+    var prisma: undefined | ReturnType<typeof prismaClient>
+}
+
+const prisma = globalThis.prisma ?? prismaClient()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
