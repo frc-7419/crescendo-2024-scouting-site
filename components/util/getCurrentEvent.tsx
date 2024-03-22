8a7b69058f3b91bed43decb90dfba33bc1879bcc
QLS <qls7419leadership@gmail.com>
2024-03-21 20:40:06 -0700

    goat code

--- components/util/getCurrentEvent.tsx
+++ components/util/getCurrentEvent.tsx
@@ -4,14 +4,18 @@ export const getCurrentEvent = () => {
     const mar11 = new Date('2024-03-11');
     const mar30 = new Date('2024-03-30');
 
-    // if (currentDate > mar30) {
-    //     return '2024cabe';
-    // } else if (currentDate > mar11) {
-    //     return '2024azgl';
-    // } else if (currentDate > feb24) {
-    //     return '2024casj';
-    // } else {
-    //     return '2024casj';
-    // }
-    return '2024azgl';
-};
+        const dateMap: { [key: string]: string } = {
+            '2024-03-30': '2024cabe',
+            '2024-03-11': '2024azgl',
+            '2024-02-24': '2024casj',
+        };
+        let result = '2024casj'; // silicon
+        for (const dateStr in dateMap) {
+            const date = new Date(dateStr);
+            if (currentDate > date) {
+                result = dateMap[dateStr];
+                break;
+            }
+        }
+        return result;
+    };
