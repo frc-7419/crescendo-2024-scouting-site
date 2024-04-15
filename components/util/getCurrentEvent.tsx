2acc6be5d8711aef15c0c0c90c8a6384feb0a567
MostOriginalIGN <astrogamer543@gmail.com>
2024-04-14 22:59:08 -0700

    jawn

--- components/util/getCurrentEvent.tsx
+++ components/util/getCurrentEvent.tsx
@@ -2,10 +2,10 @@ export const getCurrentEvent = () => {
     const currentDate = new Date();
 
     const dateMap: { [key: string]: string } = {
+        '2024-04-12': '2024joh',
         '2024-03-30': '2024cabe',
         '2024-03-11': '2024azgl',
         '2024-02-24': '2024casj',
-        '2024-04-12': '2024joh',
     };
     let result = '2024casj'; // silicon
     for (const dateStr in dateMap) {
